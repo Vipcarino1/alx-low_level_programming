29413fb5dfc6bd361422b0f63b004ccfbb5f9d45
Vipcarino1 <pgc00000000001@gmail.com>
2023-08-14 15:00:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,30 +5,30 @@
  */
 int main(void)
 {
-	int i = '0';
-	int j = '0';
-	while(i <= '9')
-	{
-		while(j <= '9')
-		{
-			if(!(i > j) || i == j)
-			{
-				putchar(i);
-				putchar(j);
-				if(i == '8' && j == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			j++;
-		}
-	i++;
-	j = '0';
-	}
-	return (0);
+int i = '0';
+int j = '0';
+while(i <= '9')
+{
+while(j <= '9')
+{
+if(!(i > j) || i == j)
+{
+putchar(i);
+putchar(j);
+if(i == '8' && j == '9')
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+j++;
+}
+i++;
+j = '0';
+}
+return (0);
 }
