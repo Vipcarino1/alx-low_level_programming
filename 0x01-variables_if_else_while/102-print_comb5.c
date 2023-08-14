113e74d6f7c1638bd4779f57216304d7e7d7306e
Vipcarino1 <pgc00000000001@gmail.com>
2023-08-14 16:22:59 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,11 +6,12 @@
 int main(void)
 {
 	int i, j;
+
 	for (i = 0; i <= 99; i++)
 	{
 		for (j = 0; j <= 99; j++)
 		{
-			if (i < j && i !=j)
+			if (i < j && i != j)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
