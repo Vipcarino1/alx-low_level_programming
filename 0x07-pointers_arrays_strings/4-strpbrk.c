46cabfdc73e6ef298f89472d3190ec8e0bf1adae
Vipcarino1 <pgc00000000001@gmail.com>
2023-08-28 11:07:48 -0700

    0x07 tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,9 +14,9 @@ char *_strpbrk(char *s, char *accept)
 		for (k = 0; accept[k]; k++)
 		{
 			if (*s == accept[k])
-				return(s);
+				return (s);
 		}
 		s++;
 	}
-	return('\0');
+	return ('\0');
 }
